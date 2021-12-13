0308a20b8042648abfa0cbe5cc5e319e499f509b
Kenny Barlee <kbarlee@outlook.com>
2021-12-13 15:44:53 +0000

    initial set of files

---
+++ setup_part1.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+apt update & apt upgrade -y
+apt install cmake
+
+## USRP setup?
+if (whiptail --title "Using a USRP?" --yesno "Hit yes to install USRP support" 8 78); then
+    # UHD
+    apt install libuhd-dev libuhd3.15.0 uhd-host
+    /usr/lib/uhd/utils/uhd_images_downloader.py
+fi
+
+## LimeSDR setup?
+if (whiptail --title "Using a LimeSDR?" --yesno "Hit yes to install LimeSDR support" 8 78); then
+    # SoapySDR
+    git clone https://github.com/pothosware/SoapySDR.git
+    cd SoapySDR
+    git checkout tags/soapy-sdr-0.7.2
+    mkdir build && cd build
+    cmake ..
+    make -j4
+    make install
+    ldconfig
+    # LimeSuite
+    apt install libusb-1.0-0-dev -y
+    git clone https://github.com/myriadrf/LimeSuite.git
+    cd LimeSuite
+    git checkout tags/v20.01.0
+    mkdir builddir && cd builddir
+    cmake ../
+    make -j4
+    make install
+    ldconfig
+    cd ..
+    cd udev-rules
+    ./install.sh
+fi
+
+## srsRAN
+apt install libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev -y
+git clone https://github.com/srsRAN/srsRAN.git
+cd srsRAN
+git checkout tags/release_19_12
+mkdir build && cd build
+cmake ../
+make -j4
+make install
+ldconfig
+# copy default srsRAN configs to /root
+./srslte_install_configs.sh user
+
+
+## Governor
+systemctl disable ondemand
+apt install linux-tools-raspi
+echo GOVERNOR="performance" >> /etc/default/cpufrequtils
+
+## Overclock
+if (whiptail --title "Do you want to overclock your Pi4 to 2GHz?" --yesno "Only do this if you have a heatsink. This script will edit boot configs for 'Ubuntu Pi' and 'Raspberry Pi OS'. If you are running a different distro you will need to run this manually." 8 78); >
+    . /etc/os-release
+    if [[ "$ID" == "ubuntu" ]]; then
+        echo "found Ubuntu, setting overclock config in /boot/firmware/config.txt"
+        echo -e "\nover_voltage=6\narm_freq=2000" >> /boot/firmware/config.txt
+    fi
+    if [[ "$ID" == "raspbian" ]]; then
+        echo "found Raspberry Pi OS, setting overclock config in /boot/config.txt"
+        echo -e "\nover_voltage=6\narm_freq=2000" >> /boot/config.txt
+    fi
+fi
+
+echo "Setup part 1 complete! Reboot your Pi now."
---
+++ setup_part2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+## Copy tweaked eNB config
+cp config/enb.conf /root/.config/srsenb/enb.conf
+
+## Load system services
+apt install screen -y
+cp service/* /etc/systemd/system/
+systemctl daemon reload
+
+## Run ePC?
+if (whiptail --title "Run local core?" --yesno "Hit yes to set up a local core" 8 78); >
+    # EPC config
+    if (whiptail --title "Edit the MME config" --yesno "Hit yes to edit the MME config. Make sure to set your MCC/MNC, and MME bind address)" 8 78); >
+        nano /root/.config/srsenb/epc.conf
+    fi
+    # HSS config
+    if (whiptail --title "Edit the HSS config" --yesno "Hit yes to edit the HSS config. Enter your SIM details following the example)" 8 7>
+        nano /root/.config/srsenb/user_db.csv
+    fi
+    systemctl enable srsepc
+    systemctl restart srsepc
+fi
+
+## eNB config
+if (whiptail --title "Edit the eNB config" --yesno "Hit yes to edit the eNB config. Make sure to set your MCC/MNC, MME/GTP/S1AP bind addresses, and DL EARFCN (set to a channel you are legally permitted to broadcast on)" 8 78); >
+    nano /root/.config/srsenb/enb.conf
+fi
+systemctl enable srsenb
+systemctl restart srsenb
